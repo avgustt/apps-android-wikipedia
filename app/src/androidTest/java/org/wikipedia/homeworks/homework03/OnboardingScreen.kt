0cd33671337b723fe863de4e991d201a2a244b9e
Sergei Sevostianov <avgusts14@gmail.com>
2025-02-07 13:53:20 +0000

    Задание для 3 урока, описание самых значимых элементов на странице

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework03/OnboardingScreen.kt
@@ -0,0 +1,28 @@
+package org.wikipedia.homeworks.homework03
+
+val wikiLogo = listOf(
+    AppCompatImageView::class.java,  //Класс
+    "main_toolbar_wordmark", //id
+    null, //нет текста
+)
+
+val searchBottom = listOf(
+    AppCompatImageView::class.java,  //Класс
+    null,  //Нет ID
+    null, //нет текста
+)
+
+val descriptionWikipedia = listOf(
+    MaterialTextView::class.java,   //Класс
+    "view_announcement_text", //id
+    "wikipedia_2024_review", //ID текста
+)
+
+val moreRandomArticlesButton = listOf(
+    MaterialButton::class.java, // Класс
+    "view_featured_article_card_footer", // ID кнопки
+    "more_random_articles", // ID текста
+)
+
+
+
