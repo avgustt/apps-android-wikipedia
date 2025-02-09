0ed0f35ce72ad318364ab0b0776346b0bd58ce16
Sergei Sevostianov <avgusts14@gmail.com>
2025-02-09 22:08:50 +0000

    Задание для 3 урока, описание самых значимых элементов на странице

--- app/src/androidTest/java/org/wikipedia/homeworks/homework04/ScheduleEntity.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework04/ScheduleEntity.kt
@@ -4,12 +4,4 @@ import java.time.LocalTime
 
 data class ScheduleEntity(val lesson: String, val startTime: LocalTime, val endTime: LocalTime)
 
-enum class Days {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
-}
+
